3594bd79404c3702031fca6f996baefac76fe583
megumi <819664876@qq.com>
2019-10-13 17:02:27 +0800

    initial commit

---
+++ src/main/java/com/iotat/infoscreen/InfoscreenApplication.java
@@ -0,0 +1,13 @@
+package com.iotat.infoscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoscreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoscreenApplication.class, args);
+	}
+
+}
